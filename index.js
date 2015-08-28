61f5a4d200ec2b7f7eb79f6b6c4b68d665bdc039
Russell Wolf <russell@gophilosophie.com>
2015-08-28 14:25:53 -0400

    Cleaned up source code and messages

--- index.js
+++ index.js
@@ -15,46 +15,19 @@ var args = require("yargs").argv;
 var gutil = require('gutil');
 var $ = require('gulp-load-plugins')();
 
-/*
-* main functions -----------------------
-*/
-
 function Bundle() {}
 
 Bundle.prototype.run = function() {
-  if (args.bundle) {
-    this.set(args.bundle);
-  } else {
-    this.help();
-  }
+  this.set(args.bundle);
 }
 
 Bundle.prototype.set = function(bundleid) {
-
-  this.pluginMessage();
-
   return vfs.src(['./config.xml'])
     .pipe($.xmlEditor([
-        { path: '.', attr: { 'id': bundleid } } 
+      { path: '.', attr: { 'id': bundleid } } 
     ]))
   .pipe(vfs.dest("./"));
 }
-
-/*
-* helper functions -----------------------
-*/
-
-Bundle.prototype.pluginMessage = function() {
-  gutil.log("\n\tRemember to run this before you run cordova build\n");
-}
-
-Bundle.prototype.help = function() {
-  gutil.log('\n\tUSAGE:\n\t\t$ gulp cordova --bundle=com.example.yourapp\n');
-}
-
-/*
-* module function -----------------------
-*/
 
 var init = new Bundle();
 module.exports = init.run();
